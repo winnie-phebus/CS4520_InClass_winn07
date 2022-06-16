28c5c3252126241df31cb612f3dac849cbb72a91
Alix Heudebourg <alix.heudebourg@gmail.com>
2022-06-16 18:29:36 -0400

    implemented change username

--- app/src/main/java/com/example/cs4520_inclassassignments/EditProfileActivity.java
+++ app/src/main/java/com/example/cs4520_inclassassignments/EditProfileActivity.java
@@ -1,10 +1,16 @@
 package com.example.cs4520_inclassassignments;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,12 +19,16 @@ import android.widget.ImageView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
+import com.squareup.picasso.Picasso;
 
 public class EditProfileActivity extends AppCompatActivity {
+
+    private static final String TAG = "ICO9_EDIT_PROF";
 
     EditText newUsername;
     ImageView imageDisp;
     Button takePicture, leaveNoSave, saveChanges;
+    Uri img;
     private FirebaseUser user;
 
 
@@ -26,6 +36,7 @@ public class EditProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ic09_edit_profile);
+
 
         user = FirebaseAuth.getInstance().getCurrentUser();
         newUsername = findViewById(R.id.ic09_edit_newUsernameInput);
@@ -38,6 +49,9 @@ public class EditProfileActivity extends AppCompatActivity {
         newUsername.setText(user.getDisplayName());
         // TODO fix image ressource change
         // imageDisp.setImageResource(user.getPhotoUrl());
+
+        // show avatar
+        Picasso.get().load(user.getPhotoUrl()).into(imageDisp);
 
         leaveNoSave.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,5 +73,63 @@ public class EditProfileActivity extends AppCompatActivity {
             }
         });
 
+        ActivityResultLauncher<Intent> retrieveImg =
+                registerForActivityResult(
+                        new ActivityResultContracts.StartActivityForResult(),
+                        new ActivityResultCallback<ActivityResult>() {
+                            @Override
+                            public void onActivityResult(ActivityResult result) {
+                                resultDisplay(result);
+                            }
+                        }
+                );
+
+        takePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (InClass08Activity.hasCameraPermission(EditProfileActivity.this)) {
+                    Intent openCamera = new Intent(EditProfileActivity.this, CameraControllerActivity.class);
+                    openCamera.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                    retrieveImg.launch(openCamera);
+                } else {
+                    InClass08Activity.requestPermission(EditProfileActivity.this);
+                }
+            }
+        });
+
+
+
+    }
+
+    public void resultDisplay(ActivityResult result) {
+        Log.d(TAG, "resultDisplay: " + result);
+        if (result.getResultCode() == RESULT_OK) {
+            Intent data = result.getData();
+            int takeFlags = data.getFlags() & Intent.FLAG_GRANT_READ_URI_PERMISSION;
+
+            img = data.getExtras().getParcelable(CameraControllerActivity.IMG_KEY);
+           /* this.getContentResolver().takePersistableUriPermission(img,
+                    Intent.FLAG_GRANT_READ_URI_PERMISSION);*/
+            Log.d(TAG, "resultDisplay: img:" + img);
+
+            //Intent backToSender = new Intent(String.valueOf(selectedImageUri));
+            //startActivity(backToSender);
+            // new Intent(CameraControllerActivity.class, MessageActivity.class);
+            toggleVis(true);
+            Picasso.get().load(img).fit().into(imageDisp);
+            // TODO: AESTHETIC - make ^ adjustable height w ratio
+        }
+    }
+
+    private void toggleVis(boolean isVisible) {
+        int vis;
+        if (isVisible) {
+            vis = View.VISIBLE;
+        } else {
+            vis = View.INVISIBLE;
+        }
+
+        imageDisp.clearAnimation();
+        imageDisp.setVisibility(vis);
     }
 }
